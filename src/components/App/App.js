679d2efd495b7a489abeed62e664e400a123d092
Hugo De Moraes <demoraeshugo@gmail.com>
2019-11-01 18:01:02 -0400

    Added High and Low Temps

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,7 +13,12 @@ class App extends Component {
         location: "New York",
         ID: 5128638,
         code: "US",
-        temp: 0,
+        temp: {
+          current: 0,
+          min: 0,
+          max: 0,
+        },
+        main: "",
         description: "",
         humidity: 0
       },
@@ -39,14 +44,66 @@ class App extends Component {
           location: location,
           ID: ID,
           code: code,
-          temp: this.unitConverstion(data.main.temp),
-          description: data.weather[0].main,
+          temp: {
+            current: this.unitConverstion(data.main.temp),
+            min: this.unitConverstion(data.main.temp_min),
+            max: this.unitConverstion(data.main.temp_max),
+          },
+          description: data.weather[0].description,
           humidity: data.main.humidity
         },
         formValue: this.state.formValue
       })
     );
   };
+
+  /*
+
+  {
+  "coord": {
+    "lon": -122.08,
+    "lat": 37.39
+  },
+  "weather": [
+    {
+      "id": 800,
+      "main": "Clear",
+      "description": "clear sky",
+      "icon": "01d"
+    }
+  ],
+  "base": "stations",
+  "main": {
+    "temp": 296.71,
+    "pressure": 1013,
+    "humidity": 53,
+    "temp_min": 294.82,
+    "temp_max": 298.71
+  },
+  "visibility": 16093,
+  "wind": {
+    "speed": 1.5,
+    "deg": 350
+  },
+  "clouds": {
+    "all": 1
+  },
+  "dt": 1560350645,
+  "sys": {
+    "type": 1,
+    "id": 5122,
+    "message": 0.0139,
+    "country": "US",
+    "sunrise": 1560343627,
+    "sunset": 1560396563
+  },
+  "timezone": -25200,
+  "id": 420006353,
+  "name": "Mountain View",
+  "cod": 200
+
+
+  */
 
   unitConverstion(k) {
     return Math.round(((k - 273.15) * 9) / 5 + 32);
@@ -67,17 +124,24 @@ class App extends Component {
     });
   };
 
+  
+
   handleSubmit = event => {
     var location = event.target.firstChild.firstChild.value;
     location = this.getLocation(location);
     event.preventDefault();
     if (location) {
+      const currentData = this.state.currentData;
       this.setState({
         currentData: {
           location: location.name,
           ID: location.id,
           code: location.country,
-          temp: this.state.currentData.temp,
+          temp: {
+            current: currentData.temp.current,
+            min: currentData.temp.temp_min,
+            max: currentData.temp.temp_max,
+          },
           description: this.state.currentData.description,
           humidity: this.state.currentData.humidity
         },
