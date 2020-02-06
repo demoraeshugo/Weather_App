12319121d4710dbbc744e734fd9d9674bec42a87
Hugo De Moraes <demoraeshugo@gmail.com>
2020-02-05 21:51:55 -0500

    Styling

--- src/components/Body/Current/index.js
+++ src/components/Body/Current/index.js
@@ -3,35 +3,56 @@ import "/Users/School/Desktop/CS_Projects/Weather_App/weather_app/src/Styles/sty
 import { ReactComponent as DewDropIcon } from "/Users/School/Desktop/CS_Projects/Weather_App/weather_app/src/assets/tint-solid.svg";
 import { ReactComponent as WindIcon } from "/Users/School/Desktop/CS_Projects/Weather_App/weather_app/src/assets/wind-solid.svg";
 import { ReactComponent as ThermometerIcon } from "/Users/School/Desktop/CS_Projects/Weather_App/weather_app/src/assets/thermometer-half-solid.svg";
+import { ReactComponent as CurrentWeatherIcon } from "/Users/School/Desktop/CS_Projects/Weather_App/weather_app/src/assets/cloud-sun-solid.svg";
 
 const CurrentWeather = props => {
-  const { current, max, min, description, humidity, speed } = props;
-
+  var { current } = props
+  current = Math.round(current, 2)
+  const { max, min, description, humidity, speed } = props;
   return (
     <>
-      <div className="currentWeather">
-        <div>
-          Current <span>{current}° F</span>
+      <div className="currentWeather flex-columns">
+        <div className="flex-rows" id="row-1">
+          <div className="icons">
+            <CurrentWeatherIcon />
+          </div>
+          <div>
+            <span id="currentTemp">{current}° F</span>
+          </div>
         </div>
         <div>
-          High <span>{max}° F</span>
+          <span className="description">{description}</span>
         </div>
-        <div>
-          Low <span>{min}° F</span>
-        </div>
-        <div className="icons">
-          <DewDropIcon />
-          <ThermometerIcon />
-          <WindIcon />
-        </div>
-        <div>
-          Conditions: <span>{description}</span>
-        </div>
-        <div>
-          Humidity: <span>{humidity}%</span>
-        </div>
-        <div>
-          Wind Speed: <span>{speed}mph</span>
+        <div className="flex-rows" id="row-3">
+          <div className="flex-columns row-3-columns">
+            <div className="icons">
+              <DewDropIcon />
+            </div>
+            <div>
+              <span id="humidity">{humidity}%</span>
+            </div>
+          </div>
+          <span className="line"></span>
+          <div className="flex-columns">
+            <div className="icons">
+              {" "}
+              <ThermometerIcon />
+            </div>
+            <div className="flex-rows">
+                <div id="maxTemp">{max}° F</div>
+                <div id="minTemp">{min}° F</div>
+            </div>
+          </div>
+          <span className="line"></span>
+          <div className="flex-columns">
+            <div className="icons">
+              {" "}
+              <WindIcon />
+            </div>
+            <div>
+              <span id="windSpeed">{speed}mph</span>
+            </div>
+          </div>
         </div>
       </div>
     </>
