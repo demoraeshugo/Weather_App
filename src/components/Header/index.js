b58d676363b9dfb39507c5edd7f9a7ed2f01eb84
Hugo De Moraes <demoraeshugo@gmail.com>
2020-04-07 13:02:11 -0400

    Makes API calls from suggestion box

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,54 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import "../Header/styles.css";
-import SearchBar from "./AutoSuggest/index";
+import SearchBar from "./SearchBar/index";
 //import '../../Styles/styles.css'
 
-class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      input: "",
-    };
-  }
+const Header = (props) => {
+  
+  const { getSuggestions, cityList, onSuggestionSelected } = props;
 
-  width = {
-    width: "150px",
-  };
-
-  render() {
-    const {
-      handleSubmit,
-      handleChange,
-      formValue,
-      getSuggestions,
-      cityList
-    } = this.props;
-    return (
-      <div className="navBar">
-        <span>Weatherly</span>
-        <SearchBar
-          cityList={cityList}
-          getSuggestions={getSuggestions}
-        ></SearchBar>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="navBar">
+      <span>Weatherly</span>
+      <SearchBar
+        cityList={cityList}
+        getSuggestions={getSuggestions}
+        onSuggestionSelected={onSuggestionSelected}
+      ></SearchBar>
+    </div>
+  );
+};
 
 export default Header;
-
-/*
-
- <form autoComplete="off" onSubmit={e => handleSubmit(e)}>
-          <div className="autocomplete">
-            <input
-              onChange={e => handleChange(e)}
-              value={formValue}
-              id="Location"
-              type="text"
-              name="Location"
-            ></input>
-          </div>
-          <input type="submit"></input>
-        </form>
-*/
