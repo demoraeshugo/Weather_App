fb1987cd4b5e5ed254df3950f2d107f1a40c11d8
Hugo De Moraes <demoraeshugo@gmail.com>
2020-05-27 17:16:51 -0400

    Fixed size property

--- View/src/components/Body/index.js
+++ View/src/components/Body/index.js
@@ -4,7 +4,6 @@ import CurrentWeather from "./Current/index";
 import ForecastWeather from "./Forecast/index";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 
 class Body extends Component {
   componentDidMount() {
